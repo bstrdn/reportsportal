c2a2a3c492ffe2ed02a14ce20a9734fe0759f228
bstrd <bstrdn@gmail.com>
2021-01-16 18:02:30 +0300

    0.2.2.1_mini_fix

--- src/main/java/ru/bstrdn/report/repository/JdbcReportRepository.java
+++ src/main/java/ru/bstrdn/report/repository/JdbcReportRepository.java
@@ -7,16 +7,13 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.bstrdn.report.model.Report_1;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Repository
 //@Transactional(readOnly = true)
 public class JdbcReportRepository {
     static final String FIO_PATTERN = "(\\S+\\s)(\\S{1})\\S+\\s(\\S{1})\\S+";
-
 
     private static final RowMapper<Report_1> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Report_1.class);
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(JdbcReportRepository.class);
@@ -28,37 +25,6 @@ public class JdbcReportRepository {
     }
 
 //    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
-
-//    @Override
-//    public Meal get(int id, int userId) {
-//        List<Meal> meals = jdbcTemplate.query(
-//                "SELECT * FROM meals WHERE id = ? AND user_id = ?", ROW_MAPPER, id, userId);
-//        return DataAccessUtils.singleResult(meals);
-//    }
-//
-//    @Override
-//    public List<Meal> getAll(int userId) {
-//        return jdbcTemplate.query(
-//                "SELECT * FROM meals WHERE user_id=? ORDER BY date_time DESC", ROW_MAPPER, userId);
-//    }
-
-//    List<Report_1> getUser() {
-//        return null;
-//    }
-
-    //выполнение запроса в базу
-    public void Test1() {
-        log.debug("start test");
-//        jdbcTemplate.update("insert into TEST (TESTFILD) values ( 'test1' )");
-    }
-
-    //маппинг юзеров
-//    public void Test2() {
-//        log.debug("start test");
-//        List<Report_1> list = jdbcTemplate.query("select * from USER", ROW_MAPPER);
-//        System.out.println(list);
-//    }
 
     //ПЕРВИЧНЫЕ ПАЦИЕНТЫ (ВСЕ) не уникальные пациенты
     public List<Report_1> queryReport_1(String fromDate, String toDate, String radio, Integer department, Integer registrar) {
@@ -97,7 +63,6 @@ public class JdbcReportRepository {
 
                 """);
 
-
         //фильтр по отделению
         if (department > 0) {
             stringBuilder.append(String.format(" AND doc.depnum = %d", department));
@@ -125,20 +90,18 @@ public class JdbcReportRepository {
 
 
         List<Report_1> report_1 = jdbcTemplate.query(stringBuilder.append("ORDER BY cl.fullname DESC;").toString(), ROW_MAPPER, fromDate, toDate);
-        report_1.forEach(r -> r.setDocFullname(r.getFullname().replaceAll(FIO_PATTERN, "$1$2. $3.")));
+        report_1.forEach(r -> r.setDocFullname(r.getDocFullname().replaceAll(FIO_PATTERN, "$1$2. $3.")));
         return report_1;
     }
 
-
-    public List<String> getAllRegistrar() {
-        return jdbcTemplate.queryForList("""
-                SELECT dname
-                FROM doctor
-                WHERE stdtype = 1
-                order by dname
-                """, String.class);
-    }
-
+//    public List<String> getAllRegistrar() {
+//        return jdbcTemplate.queryForList("""
+//                SELECT dname
+//                FROM doctor
+//                WHERE stdtype = 1
+//                order by dname
+//                """, String.class);
+//    }
 
     public List<Map<String, Object>> getAllRegistrarWithId() {
         List<Map<String, Object>> map;
@@ -151,24 +114,23 @@ public class JdbcReportRepository {
         return map;
     }
 
-    public List<String> getAllDepartment() {
-        return jdbcTemplate.queryForList("""
-                SELECT depname
-                FROM departments
-                WHERE depnum NOT IN (10001542)
-                AND depnum != 10001020
-                ORDER BY depname
-                """, String.class);
-    }
+//    public List<String> getAllDepartment() {
+//        return jdbcTemplate.queryForList("""
+//                SELECT depname
+//                FROM departments
+//                WHERE depnum NOT IN (10001542)
+//                AND depnum != 10001020
+//                ORDER BY depname
+//                """, String.class);
+//    }
 
     public List<Map<String, Object>> getAllDepartmentWithId() {
         List<Map<String, Object>> map;
         map = jdbcTemplate.queryForList("""
-                        SELECT depnum, depname
-                        FROM departments
-                        WHERE depnum NOT IN (10001542)
-                        AND depnum != 10001020
-                        ORDER BY depname
+                SELECT depnum, depname
+                FROM departments
+                WHERE depnum NOT IN (10001542, 10001020, 10001024)
+                ORDER BY depname
                 """);
         return map;
     }
--- src/main/java/ru/bstrdn/report/web/MainController.java
+++ src/main/java/ru/bstrdn/report/web/MainController.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.bstrdn.report.repository.JdbcReportRepository;
 
 @Data
@@ -15,13 +16,8 @@ JdbcReportRepository report;
 
     @GetMapping("/")
     public String home (Model model) {
-//        model.addAttribute("allRegistrar", report.getAllRegistrar());
         model.addAttribute("allRegistrarWithId", report.getAllRegistrarWithId());
-//        model.addAttribute("allDepartment", report.getAllDepartment());
         model.addAttribute("allDepartmentWithId", report.getAllDepartmentWithId());
-//        List<Map<String, Object>> maptest = report.departments();
-//        model.addAttribute("allDepartment2", report.departments());
-//        model.addAttribute("fioRegistratora", List.of("Иванов", "Petrov", "Андреищев"));
         model.addAttribute("reportName", "Первичные пациенты");
         return "report_1";
     }
@@ -37,6 +33,13 @@ JdbcReportRepository report;
         return "home";
     }
 
+    @GetMapping("/user")
+    public String user() {
+        return ("<h1>Welcome User</h1>");
+    }
 
-
+    @GetMapping("/admin")
+    public String admin() {
+        return ("<h1>Welcome Admin</h1>");
+    }
 }
